1c9c0c99d973ad2b090dabee453a5f8cd5c56246
Chiper Andrei <138249161+chiperandrei42@users.noreply.github.com>
2026-03-25 08:49:19 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -57,6 +57,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             Documentation
+            Test commit
           </a>
         </div>
       </main>
